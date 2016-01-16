fee16e71e49f02e9f80af5bbae2ec1e0d3e068fc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-01-15 22:40:07 -0500

    built for node 0.12

---
+++ dist/bundle.js
@@ -0,0 +1,7 @@
+// main.js
+const add = (a, b) => a + b
+
+const a = 10;
+const b = 2;
+const sum = add(a, b)
+console.log(`${a} + ${b} = ${sum}`)
---
+++ dist/compiled.js
@@ -0,0 +1,9 @@
+// main.js
+var add = function (a, b) {
+  return a + b;
+};
+
+var a = 10;
+var b = 2;
+var sum = add(a, b);
+console.log(a + " + " + b + " = " + sum);
