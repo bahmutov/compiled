0ab5cd81d81c80cfb7ce81aa72fe3e5aa2e12a74
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-01-16 16:25:59 -0500

    feat(build): build script working

--- bin/build.js
+++ bin/build.js
@@ -2,6 +2,8 @@
 
 'use strict'
 
+var debug = require('debug')('compiled')
+
 var help = [
   'USE: build <path/to/first/js/file>',
   '     build src/main.js'
@@ -15,20 +17,45 @@ require('simple-bin-help')({
 
 var inputFilename = process.argv[2]
 var outputFilename = 'dist/bundle.js'
-console.log('building from', inputFilename)
+var featuresFilename = 'dist/es6-features.json'
+debug('building from', inputFilename)
 
-var rollup = require('rollup')
-rollup.rollup({
-  entry: inputFilename
-}).then(function (bundle) {
-  bundle.write({
-    format: 'cjs',
-    dest: outputFilename
+var saveFile = require('fs').writeFileSync
+
+function roll (inputFilename, outputFilename) {
+  var rollup = require('rollup')
+
+  return rollup.rollup({
+    entry: inputFilename
+  }).then(function (bundle) {
+    return bundle.write({
+      format: 'cjs',
+      dest: outputFilename
+    }).then(function () {
+      debug('saved bundle', outputFilename)
+      return outputFilename
+    })
   })
+}
 
-  console.log('saved bundle', outputFilename)
-}).catch(function (err) {
-  console.error('problem building', inputFilename)
-  console.error(err.message)
-  process.exit(-1)
-})
+function findUsedES6 (outputFilename, filename) {
+  var testify = require('es-feature-tests/testify')
+  debug('scanning for es features', filename)
+  var output = testify.scan({
+    files: filename,
+    output: 'json',
+    enable: []
+  })
+  debug('used ES features', output)
+
+  saveFile(outputFilename, JSON.stringify(output, null, 2), 'utf-8')
+  debug('saved file with found es features', outputFilename)
+}
+
+roll(inputFilename, outputFilename)
+  .then(findUsedES6.bind(null, featuresFilename))
+  .catch(function (err) {
+    console.error('problem building', inputFilename)
+    console.error(err.message)
+    process.exit(-1)
+  })
