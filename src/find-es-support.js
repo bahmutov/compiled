c2fa268b4a71e2e116b60c71fe7c1e9b6552993c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-01-20 16:46:21 -0500

    chore(find): not using promises when finding es features manually

--- src/find-es-support.js
+++ src/find-es-support.js
@@ -1,17 +1,25 @@
+/*
+To use:
+    nvm 0.12
+    node find-es-support.js > es.for.0.12.json
+*/
 var es6support = require('es-feature-tests')
+var findES6Support
 
-function findES6Support () {
-  return new Promise(function (resolve) {
-    es6support('all', resolve)
-  })
+if (typeof Promise === 'undefined') {
+  findES6Support = function findES6Support () {
+    return new Promise(function (resolve) {
+      es6support('all', resolve)
+    })
+  }
+} else {
+  findES6Support = es6support
 }
 
 module.exports = findES6Support
 
 if (!module.parent) {
-  findES6Support()
-    .then(function (features) {
-      console.log(JSON.stringify(features, null, 2))
-    })
-    .catch(console.error.bind(console))
+  es6support('all', function (features) {
+    console.log(JSON.stringify(features, null, 2))
+  })
 }
